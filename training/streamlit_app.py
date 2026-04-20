88092b29b17897e7c7ab4bbe33ef49613680e242
Eben Ernst <elernst@shoprite.co.za>
2026-04-20 12:54:50 +0200

    added the correct way to reference it

--- training/streamlit_app.py
+++ training/streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests  
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -17,9 +18,10 @@ st.write('The name on your Smoothie will be: ', name_on_order)
 # session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
-
+pd_Df = my_dataframe.to_pandas()
+st.dataframe(data=pd_Df, use_container_width=True)
 
 
 ingredients_list = st.multiselect(
@@ -34,9 +36,13 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+' '
+        
+        search_on=pd_Df.loc[pd_Df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen+" Nutrition Information")
         small_lettered_fruit = fruit_chosen.lower()
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{small_lettered_fruit}")
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         # st.text(smoothiefroot_response.json())
         sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
 
